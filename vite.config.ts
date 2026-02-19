206b6812c35f2d3fa5cf621ddd5508d66f8a4033
Mohamed Sebbar <msebbar.00@gmail.com>
2026-02-18 22:43:11 -0500

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio/",
   server: {
     host: "::",
     port: 8080,
